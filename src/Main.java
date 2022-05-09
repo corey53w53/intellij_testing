4656387dd6a9ac3d314f2722b055e7a26f3a3932
Corey Wang <corefire2004@gmail.com>
2022-05-09 01:57:36 -0400

    input

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Person(12);
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        String string_age = s.nextLine();
+        System.out.println("My type is " + string_age.getClass());
+        int age = Integer.parseInt(string_age);
+        Person(age);
     }
 
     public static void Person(int age) {
