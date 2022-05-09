84332ed66aa5310b719ccbda35ca5fab3b254161
Corey Wang <corefire2004@gmail.com>
2022-05-09 01:41:43 -0400

    init

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("53W53");
-        System.out.println("github");
+        System.out.println("github test");
     }
 }
