1f777da21ddfafe24d3745b864a145f46ae2d7dd
Corey Wang <corefire2004@gmail.com>
2022-05-10 17:47:00 -0400

    input

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,16 @@
+import java.io.FileNotFoundException;
 import java.util.Scanner;
-
+import java.io.File;
 public class Main {
-    public static void main(String[] args) {
-//        System.out.println(Person());
-//        split("1ab23c");
-
-        System.out.println(is_palindrome("hellolleh"));
-        System.out.println(is_palindrome("12321"));
-        System.out.println(is_palindrome("1"));
-        System.out.println(is_palindrome("12"));
-        System.out.println(is_palindrome("123"));
-    }
-    public static boolean is_palindrome(String s){
-        for (int i=0;i<s.length()/2;i++){
-            if (s.charAt(i) != s.charAt(s.length() - i - 1)) {return false;}
+    public static void main(String[] args) throws FileNotFoundException {
+        File myObj = new File("input.txt");
+        Scanner myReader = new Scanner(myObj);
+        String input="";
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            input=input+data+"\n";
         }
-        return true;
-    }
-    public static String Person() {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter your age:");
-        String string_age = s.nextLine();
-        int age = Integer.parseInt(string_age);
-        return "I am " + age + " years old.";
-    }
-    public static void split(String s){
-        String ints="";
-        String chars="";
-        for (int i=0;i<s.length();i++){
-            String current_char="";
-            try{
-                Integer.parseInt(Character.toString(s.charAt(i)));
-                ints=ints.concat(Character.toString(s.charAt(i)));
-            }
-            catch(Exception e){
-                chars=chars.concat(Character.toString(s.charAt(i)));
-            }
-        }
-        String[] r= {ints,chars};
-        //yo how tf does this vvv for loop work, plz help
-        for (String i : r) {
-            System.out.println(i);
-        }
-        //for loop above and below are the same, whatttt
-//        for (int i=0;i<myarr.length;i++){
-//            System.out.println(myarr[i]);
-//        }
-
+        myReader.close();
+        System.out.println(input);
     }
 }
