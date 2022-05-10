526b3db7f77b8599627c6b6b04fcc6be30302889
Corey Wang <corefire2004@gmail.com>
2022-05-10 18:10:30 -0400

    finished day 1, p1 and p2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.io.File;
 public class Main {
@@ -6,9 +8,24 @@ public class Main {
         File myObj = new File("input.txt");
         Scanner myReader = new Scanner(myObj);
         String input="";
-        while (myReader.hasNextLine()) {input=input+myReader.nextLine()+"\n";}
+        List<Integer> list=new ArrayList<Integer>();
+        while (myReader.hasNextLine()){
+            list.add(Integer.parseInt(myReader.nextLine()));
+        }
         myReader.close();
-        System.out.println(input);
-
+        int counter1=0;
+        for (int i=0;i<list.size()-1;i++){
+            if (list.get(i+1)-list.get(i)>0){
+                counter1+=1;
+            }
+        }
+        System.out.println("part one: " + counter1);
+        int counter2=0;
+        for (int i=0;i<list.size()-3;i++){
+            if (list.get(i+3)-list.get(i)>0){
+                counter2+=1;
+            }
+        }
+        System.out.println("part two: " + counter2);
     }
 }
